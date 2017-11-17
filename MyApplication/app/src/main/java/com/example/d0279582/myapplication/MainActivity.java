418d4f914b3c0d462a28f11e73d9f0cc83eaf94c
Andreas Badurczyk <andreas.badurczyk@dm.de>
2017-11-17 14:58:24 +0100

    nfc stuff

--- MyApplication/app/src/main/java/com/example/d0279582/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/d0279582/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.d0279582.myapplication;
 
 import android.content.Intent;
+import android.nfc.FormatException;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.NdefFormatable;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,7 +17,9 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import android.app.Activity;
@@ -35,56 +40,98 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "NfcDemo";
 
-    private TextView mTextView;
-    private NfcAdapter mNfcAdapter;
-
-
-    public static final String MIME_TEXT_PLAIN = "text/plain";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        if (mNfcAdapter == null) {
-            // Stop here, we definitely need NFC
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
-            finish();
+        mTextView = (TextView) findViewById(R.id.textView_explanation);
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mAdapter == null) {
+            //nfc not support your device.
             return;
-
         }
+        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
-        handleIntent(getIntent());
     }
 
-    private void handleIntent(Intent intent) {
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+    NfcAdapter mAdapter;
+    PendingIntent mPendingIntent;
 
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
 
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAdapter != null) {
+            mAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+        String str = new String(id, StandardCharsets.US_ASCII);
+        mTextView.setText("Read content: " +  id);
 
 
-            } else {
-                Log.d(TAG, "Wrong mime type: " + type);
-            }
-        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+     //  GetDataFromTag(tag, intent);
 
-            // In case we would still use the Tech Discovered Intent
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String[] techList = tag.getTechList();
-            String searchedTech = Ndef.class.getName();
+    }
 
-            for (String tech : techList) {
-                if (searchedTech.equals(tech)) {
-                    break;
+    private void GetDataFromTag(Tag tag, Intent intent) {
+        Ndef ndef = Ndef.get(tag);
+        if (ndef == null) {
+            // NDEF is not supported by this Tag.
+            mTextView.setText("Read content: null" );
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+            if (ndefFormatable != null) {
+                // initialize tag with new NDEF message
+                try {
+                    ndefFormatable.connect();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        ndefFormatable.close();
+                    } catch (Exception e) {}
                 }
             }
+        }
+        try {
+            ndef.connect();
+//            txtType.setText(ndef.getType().toString());
+//            txtSize.setText(String.valueOf(ndef.getMaxSize()));
+//            txtWrite.setText(ndef.isWritable() ? "True" : "False");
+            Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            if (messages != null) {
+                NdefMessage[] ndefMessages = new NdefMessage[messages.length];
+                for (int i = 0; i < messages.length; i++) {
+                    ndefMessages[i] = (NdefMessage) messages[i];
+                }
+                NdefRecord record = ndefMessages[0].getRecords()[0];
+
+                byte[] payload = record.getPayload();
+                String text = new String(payload);
+               mTextView.setText("Read content: " + text);
+                Log.e("tag", "vahid" + text);
+                ndef.close();
+
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Cannot Read From Tag."+e, Toast.LENGTH_LONG).show();
+        }
     }
-}}
+    private TextView mTextView;
+
+}
