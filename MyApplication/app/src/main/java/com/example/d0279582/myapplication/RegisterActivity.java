942fd16f55c64e587edf2167abee0c3966591be6
Chrostek, Helge <helge.chrostek@dm.de>
2017-11-17 17:26:54 +0100

    Removed a lot of unused stuff, renamed activity

---
+++ MyApplication/app/src/main/java/com/example/d0279582/myapplication/RegisterActivity.java
@@ -0,0 +1,41 @@
+package com.example.d0279582.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A register screen that offers registering with a name.
+ */
+public class RegisterActivity extends AppCompatActivity {
+    // UI references.
+    private EditText mNameView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        // Set up the login form.
+        mNameView = (EditText) findViewById(R.id.name);
+
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+    }
+
+    private void registerUser(){
+        //TODO: Do registration of new user
+    }
+}
+
